eb3bd9aa70d08db97d6592fff3c26f978c770d19
Anisse Astier <anisse@astier.eu>
2022-12-01 08:57:13 +0100

    day01: part 1

---
+++ day01/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let elfcalories = parse(include_str!("../input.txt"));
+    //part 1
+    let res = max_calories(&elfcalories);
+    println!("Max calories: {}", res);
+    //part 2
+    //let res = max_calories2(&things);
+    //println!("Summary2: {}", res);
+}
+fn parse(input: &str) -> Vec<Vec<usize>> {
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|x| x.parse().expect("not int")).collect())
+        .collect()
+}
+fn max_calories(elfcalories: &[Vec<usize>]) -> usize {
+    elfcalories
+        .iter()
+        .map(|v| v.iter().sum())
+        .max()
+        .expect("no max")
+}
+
+#[test]
+fn test() {
+    let elfcalories = parse(include_str!("../sample.txt"));
+    //part 1
+    let res = max_calories(&elfcalories);
+    assert_eq!(res, 24000);
+    //part 2
+    // let res = max_calories2(&things);
+    // assert_eq!(res, 42);
+}
