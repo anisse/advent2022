36b5132b4dae868d76e2b4ca54bc8ec25b5e113c
Anisse Astier <anisse@astier.eu>
2022-12-04 08:43:40 +0100

    day04: part 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -9,18 +9,32 @@ fn main() {
     //let res = operation2(&sections);
     //println!("Summary2: {}", res);
 }
-fn parse(input: &str) -> Vec<(RangeInclusive<u32>, RangeInclusive<u32>)> {
-    input.lines().map(|x| x.parse().expect("not int")).collect()
+fn parse(input: &str) -> Vec<[RangeInclusive<u32>; 2]> {
+    input
+        .lines()
+        .map(|l| {
+            l.split(',')
+                .map(|r| {
+                    let v: Vec<u32> = r
+                        .split('-')
+                        .map(|i| i.parse::<u32>().expect("not int"))
+                        .collect();
+                    *v.first().expect("No first element")..=*v.get(1).expect("No second element")
+                })
+                .collect::<Vec<_>>()
+                .try_into()
+                .expect("Not two elements")
+        })
+        .collect()
 }
-fn count_overlap(sections: &[(RangeInclusive<u32>, RangeInclusive<u32>)]) -> usize {
-    let mut count = 0;
-    for _ in sections.iter() {
-        if true {
-            count += 1
-        }
-        todo!()
-    }
-    count
+fn count_overlap(sections: &[[RangeInclusive<u32>; 2]]) -> usize {
+    sections
+        .iter()
+        .filter(|ranges| {
+            (ranges[0].contains(ranges[1].start()) && ranges[0].contains(ranges[1].end()))
+                || (ranges[1].contains(ranges[0].start()) && ranges[1].contains(ranges[0].end()))
+        })
+        .count()
 }
 
 #[test]
