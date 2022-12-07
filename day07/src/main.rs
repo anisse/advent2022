a236fbafa8ad3b62663c3eacabf78fa1da6d5da2
Anisse Astier <anisse@astier.eu>
2022-12-07 07:28:39 +0100

    day07: part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -15,7 +15,8 @@ fn main() {
     let commands = parse(include_str!("../input.txt"));
     dbg!(&commands);
     //part 1
-    let res = operation(&commands);
+    let res = count_small_dirs(&commands);
+    dbg!(&res);
     println!("Summary: {}", res);
     //part 2
     //let res = operation2(&commands);
@@ -60,15 +61,103 @@ fn parse(input: &str) -> Vec<Cmd> {
     }
     cmds
 }
-fn operation(commands: &[Cmd]) -> usize {
-    let mut count = 0;
-    for _ in commands.iter() {
-        if true {
-            count += 1
+
+#[derive(Debug)]
+struct DirSize {
+    name: String,
+    size: usize,
+}
+
+#[derive(Debug)]
+struct Leaf<T> {
+    children: Vec<usize>,
+    parent: usize,
+    el: T,
+}
+
+#[derive(Debug)]
+struct Tree<T> {
+    elements: Vec<Leaf<T>>,
+}
+
+/*
+impl<T> Iterator for Tree<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {}
+}
+*/
+
+fn eval_tree(commands: &[Cmd]) -> Tree<DirSize> {
+    let mut elements: Vec<Leaf<DirSize>> = Vec::new();
+    //let mut current_dir = "".to_string();
+    let mut parent_id = 0;
+    let mut current_id = 0;
+    for cmd in commands.iter() {
+        dbg!(&cmd);
+        match cmd {
+            Cmd::Cd { target } => {
+                if target == ".." {
+                    assert!(
+                        parent_id != current_id,
+                        "Going up too much ? {}, {}, status: {:?}",
+                        current_id,
+                        parent_id,
+                        &elements,
+                    );
+                    // go up
+                    (parent_id, current_id) = (elements[parent_id].parent, parent_id);
+                } else {
+                    //current_dir = target.clone();
+                    elements.push(Leaf::<DirSize> {
+                        children: Vec::new(),
+                        el: DirSize {
+                            name: target.clone(),
+                            size: 0,
+                        },
+                        parent: current_id,
+                    });
+                    let next = elements.len() - 1;
+                    if parent_id != current_id {
+                        //already passed root
+                        elements[parent_id].children.push(next);
+                    }
+                    parent_id = current_id;
+                    current_id = next;
+                    dbg!(parent_id);
+                    dbg!(current_id);
+                    dbg!(&target);
+                }
+            }
+            Cmd::Ls { files } => {
+                files.iter().for_each(|f| match f {
+                    File::File { size, .. } => {
+                        let mut x = current_id;
+                        loop {
+                            dbg!(size);
+                            dbg!(&elements[x].el.name);
+                            elements[x].el.size += size;
+                            if x == 0 {
+                                break;
+                            }
+                            x = elements[x].parent;
+                        }
+                    }
+                    File::Dir { .. } => {}
+                });
+            }
         }
-        todo!()
     }
-    count
+    Tree { elements }
+}
+fn count_small_dirs(commands: &[Cmd]) -> usize {
+    let tree = eval_tree(commands);
+    dbg!(&tree);
+    tree.elements
+        .iter()
+        .map(|ds| ds.el.size)
+        .filter(|s| *s < 100000)
+        .sum()
 }
 
 #[test]
@@ -76,8 +165,8 @@ fn test() {
     let commands = parse(include_str!("../sample.txt"));
     dbg!(&commands);
     //part 1
-    let res = operation(&commands);
-    assert_eq!(res, 42);
+    let res = count_small_dirs(&commands);
+    assert_eq!(res, 95437);
     //part 2
     // let res = operation2(&commands);
     // assert_eq!(res, 42);
