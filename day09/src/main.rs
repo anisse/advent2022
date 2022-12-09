6f4a63af21ef82e0edc7979e0f8a3333fc693cca
Anisse Astier <anisse@astier.eu>
2022-12-09 07:36:26 +0100

    day09: part 2 was harder since part 1 was incorrect
    (but working for simple case)

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
     let res = count_tail_pos(&moves);
     println!("Summary: {}", res);
     //part 2
-    //let res = operation2(&moves);
-    //println!("Summary2: {}", res);
+    let res = count_new_tail(&moves);
+    println!("Summary2: {}", res);
 }
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum Move {
@@ -79,6 +79,42 @@ fn is_adjacent(head: &Pos, tail: &Pos) -> bool {
     (-1..=1).contains(&xdiff) && (-1..=1).contains(&ydiff)
 }
 
+fn count_new_tail(moves: &[(Move, u8)]) -> usize {
+    let mut pos = [Pos::default(); 10];
+    let mut tailpos: HashMap<Pos, ()> = HashMap::new();
+    moves.iter().for_each(|(mov, count)| {
+        (0..*count).for_each(|_| {
+            simulate_new_move(mov.clone(), &mut pos);
+            //println!("After move {mov:?}: {pos:?}");
+            tailpos.insert(pos[9], ());
+        });
+    });
+    tailpos.len()
+}
+
+fn simulate_new_move(mov: Move, positions: &mut [Pos; 10]) {
+    let (xdir, ydir) = match mov {
+        Up => (0, -1),
+        Down => (0, 1),
+        Left => (-1, 0),
+        Right => (1, 0),
+    };
+    positions[0].x += xdir;
+    positions[0].y += ydir;
+    (0..positions.len())
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .for_each(|s| {
+            let (hi, ti) = (s[0], s[1]);
+            if !is_adjacent(&positions[hi], &positions[ti]) {
+                let xdiff = positions[hi].x - positions[ti].x;
+                let ydiff = positions[hi].y - positions[ti].y;
+                positions[ti].x += xdiff.signum();
+                positions[ti].y += ydiff.signum();
+            }
+        })
+}
+
 #[test]
 fn test() {
     let moves = parse(include_str!("../sample.txt"));
@@ -86,6 +122,18 @@ fn test() {
     let res = count_tail_pos(&moves);
     assert_eq!(res, 13);
     //part 2
-    // let res = operation2(&moves);
-    // assert_eq!(res, 42);
+    let res = count_new_tail(&moves);
+    assert_eq!(res, 1);
+    let moves = parse(
+        "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20",
+    );
+    let res = count_new_tail(&moves);
+    assert_eq!(res, 36);
 }
