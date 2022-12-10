3d258978b13ad8c52aa74dd1b0c9c3d95fb01641
Anisse Astier <anisse@astier.eu>
2022-12-10 18:18:02 +0100

    day10: simplify
    Parse now returns an iterator.
    
    We got rid of all the CPU state and this solution is done in a single
    iterator chain from string to result.
    
    One iterator chain per part, so technically we parse twice.
    
    The big difference is that we don't need to emulate a CPU. noop have no
    observable effect, so they are equivalent to an "add 0". addx takes two
    cyles, so they are equivalent to "add 0, add X".
    
    X register state is done in an iterator scan() pass.
    
    Taking this simplification into account, the only issue is to have a
    proper cycle counter. The pipeline length means our simplification needs
    to be offsets by two in part 1, and that we need to insert one at the
    front and drop the last value in part 2.
    
    We do lose some tests, but there's less to test now that there is no
    CPU + cycle emulation.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,136 +1,47 @@
-use crate::Ins::*;
-
 fn main() {
-    let instructions = parse(include_str!("../input.txt"));
     //part 1
-    let res = signal_strength(&instructions);
+    let res = signal_strength_simple(include_str!("../input.txt"));
     println!("Signal strength: {}", res);
     //part 2
-    let res = crt(&instructions);
+    let res = crt_simple(include_str!("../input.txt"));
     println!("CRT:\n{}", res);
 }
-fn parse(input: &str) -> Vec<Ins> {
-    input
-        .lines()
-        .map(|x| match &x[..4] {
-            "noop" => Noop,
-            "addx" => Addx(x[5..].parse().expect("not int")),
-            _ => panic!("not instruction"),
-        })
-        .collect()
+
+fn parse(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input.lines().flat_map(|x| match &x[..4] {
+        "noop" => vec![0_i32],
+        "addx" => vec![0, x[5..].parse().expect("not int")],
+        _ => panic!("not instruction"),
+    })
 }
 
-#[derive(Debug)]
-enum Ins {
-    Noop,
-    Addx(i8),
-}
-fn signal_strength(instructions: &[Ins]) -> i32 {
-    let mut s = State::default();
-    instructions
-        .iter()
-        .flat_map(|_| {
-            let pc_start = s.pc;
-            let mut res = Vec::new();
-            while s.pc == pc_start {
-                run_cycle(instructions, &mut s);
-                /*
-                println!(
-                    "pc: {:02} {:?} -> {:03} (waiting: {:02})",
-                    s.pc,
-                    instructions[s.pc % instructions.len()],
-                    s.x,
-                    s.wait_add,
-                );
-                */
-                res.push(s.x)
-            }
-            res
+fn signal_strength_simple(input: &str) -> i32 {
+    parse(input)
+        .scan(1, |x, add| {
+            *x += add;
+            Some(*x)
         })
         .enumerate()
-        .map(|(i, x)| (i + 1, x))
-        .filter(|(i, _)| *i >= 20 && ((*i - 20) % 40) == 0)
+        .map(|(i, x)| (i + 2, x))
         /*
         .filter(|(i, x)| {
             println!("{i}: {x}");
             true
         })
         */
+        .filter(|(i, _)| *i >= 20 && ((*i - 20) % 40) == 0)
         .map(|(i, x)| (i as i32) * x)
         .sum()
 }
 
-#[derive(Debug)]
-struct State {
-    x: i32,
-    pc: usize,
-    wait: u8,
-    wait_add: i8,
-}
-impl Default for State {
-    fn default() -> Self {
-        State {
-            x: 1,
-            pc: 0,
-            wait: 0,
-            wait_add: 0,
-        }
-    }
-}
-
-fn run_cycle(instructions: &[Ins], s: &mut State) {
-    if s.wait_add != 0 {
-        s.x += s.wait_add as i32;
-        s.wait_add = 0;
-    }
-    if s.pc >= instructions.len() {
-        return;
-    }
-    match instructions[s.pc] {
-        Noop => {
-            s.pc += 1;
-        }
-        Addx(i) => {
-            if s.wait == 0 {
-                s.wait = 1;
-                return;
-            }
-            if s.wait > 0 {
-                s.wait -= 1;
-                if s.wait == 0 {
-                    // now we can run
-                    s.wait_add = i;
-                    s.pc += 1;
-                }
-            }
-        }
-    }
-}
-
-fn crt(instructions: &[Ins]) -> String {
-    let mut s = State::default();
-    instructions
-        .iter()
-        .flat_map(|_| {
-            let pc_start = s.pc;
-            let mut res = Vec::new();
-            while s.pc == pc_start {
-                run_cycle(instructions, &mut s);
-                /*
-                println!(
-                    "pc: {:02} {:?} -> {:03} (waiting: {:02})",
-                    s.pc,
-                    instructions[s.pc % instructions.len()],
-                    s.x,
-                    s.wait_add,
-                );
-                */
-                res.push(s.x)
-            }
-            res
+fn crt_simple(input: &str) -> String {
+    [0].into_iter()
+        .chain(parse(input))
+        .scan(1, |x, add| {
+            *x += add;
+            Some(*x)
         })
         .enumerate()
-        //.map(|(i, x)| (i + 1, x))
         .map(|(i, x)| (i, (x - 1..=x + 1).contains(&(i as i32 % 40))))
         .map(|(i, val)| (i, if val { '#' } else { '.' }))
         .flat_map(|(i, val)| {
@@ -140,35 +51,17 @@ fn crt(instructions: &[Ins]) -> String {
                 vec![val]
             }
         })
+        .take(41 * 6 - 1)
         .collect()
 }
 
 #[test]
 fn test() {
-    let instructions = parse(
-        "noop
-addx 3
-addx -5",
-    );
-    let mut s = State::default();
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, 1);
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, 1);
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, 1);
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, 4);
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, 4);
-    run_cycle(&instructions, &mut s);
-    assert_eq!(s.x, -1);
-    let instructions = parse(include_str!("../sample.txt"));
-    //part 1
-    let res = signal_strength(&instructions);
-    assert_eq!(res, 13140);
+    //part 1 simple
+    let res = signal_strength_simple(include_str!("../sample.txt"));
+    assert_eq!(res, 13140, "simple version");
     //part 2
-    let res = crt(&instructions);
+    let res = crt_simple(include_str!("../sample.txt"));
     assert_eq!(
         res,
         "##..##..##..##..##..##..##..##..##..##..
