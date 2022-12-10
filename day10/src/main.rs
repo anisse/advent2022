f6b6eb1925153ab0493f107ef033f3bb26dccf59
Anisse Astier <anisse@astier.eu>
2022-12-10 07:09:42 +0100

    day10: part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -20,23 +20,119 @@ fn parse(input: &str) -> Vec<Ins> {
         .collect()
 }
 
+#[derive(Debug)]
 enum Ins {
     Noop,
     Addx(i8),
 }
-fn signal_strength(instructions: &[Ins]) -> usize {
-    let mut count = 0;
-    for _ in instructions.iter() {
-        if true {
-            count += 1
-        }
-        todo!()
+fn signal_strength(instructions: &[Ins]) -> i32 {
+    let mut s = State::default();
+    instructions
+        .iter()
+        .flat_map(|_| {
+            let pc_start = s.pc;
+            let mut res = Vec::new();
+            while s.pc == pc_start {
+                run_cycle(instructions, &mut s);
+                /*
+                println!(
+                    "pc: {:02} {:?} -> {:03} (waiting: {:02})",
+                    s.pc,
+                    instructions[s.pc % instructions.len()],
+                    s.x,
+                    s.wait_add,
+                );
+                */
+                res.push(s.x)
+            }
+            res
+        })
+        .enumerate()
+        .map(|(i, x)| (i + 1, x))
+        .filter(|(i, _)| *i >= 20 && ((*i - 20) % 40) == 0)
+        .filter(|(i, x)| {
+            println!("{i}: {x}");
+            true
+        })
+        .map(|(i, x)| (i as i32) * x)
+        .sum()
+}
+/*
+fn ins_states(ins: &Ins, x: i32) -> Vec<i32> {
+    match ins {
+        Noop => vec![x],
+        Addx(i) => vec![x, x + (*i as i32)],
     }
-    count
+}
+*/
+
+#[derive(Debug)]
+struct State {
+    x: i32,
+    pc: usize,
+    wait: u8,
+    wait_add: i8,
+}
+impl Default for State {
+    fn default() -> Self {
+        State {
+            x: 1,
+            pc: 0,
+            wait: 0,
+            wait_add: 0,
+        }
+    }
+}
+
+fn run_cycle(instructions: &[Ins], s: &mut State) {
+    if s.wait_add != 0 {
+        s.x += s.wait_add as i32;
+        s.wait_add = 0;
+    }
+    if s.pc >= instructions.len() {
+        return;
+    }
+    match instructions[s.pc] {
+        Noop => {
+            s.pc += 1;
+        }
+        Addx(i) => {
+            if s.wait == 0 {
+                s.wait = 1;
+                return;
+            }
+            if s.wait > 0 {
+                s.wait -= 1;
+                if s.wait == 0 {
+                    // now we can run
+                    s.wait_add = i;
+                    s.pc += 1;
+                }
+            }
+        }
+    }
 }
 
 #[test]
 fn test() {
+    let instructions = parse(
+        "noop
+addx 3
+addx -5",
+    );
+    let mut s = State::default();
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, 1);
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, 1);
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, 1);
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, 4);
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, 4);
+    run_cycle(&instructions, &mut s);
+    assert_eq!(s.x, -1);
     let instructions = parse(include_str!("../sample.txt"));
     //part 1
     let res = signal_strength(&instructions);
