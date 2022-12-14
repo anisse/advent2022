0ae902aebb7f7218a90f78d019c9aa7a2b183803
Anisse Astier <anisse@astier.eu>
2022-12-14 06:08:53 +0100

    day14: initial parsing

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,16 +1,37 @@
 fn main() {
     let rocklines = parse(include_str!("../input.txt"));
     //part 1
-    let res = operation(&rocklines);
+    let res = max_caught_sand(&rocklines);
     println!("Summary: {}", res);
     //part 2
-    //let res = operation2(&rocklines);
+    //let res = max_caught_sand2(&rocklines);
     //println!("Summary2: {}", res);
 }
-fn parse(input: &str) -> Vec<u8> {
-    input.lines().map(|x| x.parse().expect("not int")).collect()
+
+struct Pos {
+    x: u16,
+    y: u16,
 }
-fn operation(rocklines: &[u8]) -> usize {
+
+type Line = Vec<Pos>;
+
+fn parse(input: &str) -> Vec<Line> {
+    input
+        .lines()
+        .map(|l| {
+            l.split(" -> ")
+                .map(|p| {
+                    let mut pi = p.split(',');
+                    Pos {
+                        x: pi.next().expect("no x").parse().expect("not int"),
+                        y: pi.next().expect("no y").parse().expect("not int"),
+                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+fn max_caught_sand(rocklines: &[Line]) -> usize {
     let mut count = 0;
     for _ in rocklines.iter() {
         if true {
@@ -25,9 +46,9 @@ fn operation(rocklines: &[u8]) -> usize {
 fn test() {
     let rocklines = parse(include_str!("../sample.txt"));
     //part 1
-    let res = operation(&rocklines);
+    let res = max_caught_sand(&rocklines);
     assert_eq!(res, 24);
     //part 2
-    // let res = operation2(&rocklines);
+    // let res = max_caught_sand2(&rocklines);
     // assert_eq!(res, 42);
 }
