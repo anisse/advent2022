c982cc42ec99d43128b80a479bcc388ab35052db
Anisse Astier <anisse@astier.eu>
2022-12-18 19:41:42 +0100

    day16: reimplement part 2 with a bitmask technique and single full iteration
    This is inspired by other solutions and only needs a single full
    exploration, no dual-state.
    Potential valves (with flow) are indexed, and then used as part of a
    bitmap so that we can find after the iteration the optimal path for both
    the elephant and self.
    
    With this different algorithm, we can now pass even the community test
    cases. The ValveName implementation, the path finding with memoization
    are kept. (although it's now obvious that Floyd-Warshall would be better
    here).

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -2,11 +2,10 @@ use advent2022::*;
 #[macro_use]
 extern crate scan_fmt;
 
+use std::cmp::max;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::VecDeque;
-
-use itertools::Itertools;
 
 fn main() {
     let valves = parse(input!());
@@ -14,11 +13,11 @@ fn main() {
     let res = most_30m_pressure(&valves);
     println!("Max flow in 30m: {}", res);
     //part 2
-    let res = max_flow_with_elephant(&valves);
+    let res = max_flow_with_elephant_fast(&valves);
     println!("Max flow in 26m with elephant: {}", res);
 }
 
-#[derive(Eq, PartialEq, Hash, Clone, Copy)]
+#[derive(Eq, PartialEq, PartialOrd, Ord, Hash, Clone, Copy)]
 struct ValveName(u16);
 impl std::fmt::Display for ValveName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -115,43 +114,10 @@ fn most_30m_pressure(valves: &HashMap<ValveName, Valve>) -> usize {
         println!("From AA to reach {name} (flow:{flow}), path has len {path}",);
     });
     */
-    let mut remain: VecDeque<ValveName> = valves_with_flow.iter().map(|(v, _)| *v).collect();
+    let mut remain: VecDeque<ValveName> = valves_with_flow.keys().cloned().collect();
     let mut path_memo = HashMap::new();
 
     max_flow(valves, "AA".into(), &mut remain, 30, &mut path_memo)
-}
-fn max_flow_with_elephant(valves: &HashMap<ValveName, Valve>) -> usize {
-    let valves_with_flow: HashMap<ValveName, usize> = valves
-        .iter()
-        .filter(|(_, v)| v.flow > 0)
-        .map(|(name, v)| (*name, v.flow))
-        .collect();
-
-    /*
-    valves_with_flow.iter().for_each(|(name, flow)| {
-        let path = path_to_valve(valves, "AA".into(), *name);
-        println!("From AA to reach {name} (flow:{flow}), path has len {path}",);
-    });
-    */
-    let remain: Vec<ValveName> = valves_with_flow.keys().copied().collect();
-    let mut path_memo = HashMap::new();
-
-    max_flow_double(
-        valves,
-        &[
-            Pos {
-                at: "AA".into(),
-                moving: None,
-            },
-            Pos {
-                at: "AA".into(),
-                moving: None,
-            },
-        ],
-        &remain,
-        26,
-        &mut path_memo,
-    )
 }
 
 fn max_flow(
@@ -209,14 +175,14 @@ fn max_flow(
     max
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Movement {
     dest: ValveName,
     flow: usize,
     cost: u8,
 }
 
-//#[derive()]
+#[derive(PartialEq, Eq, Hash, Clone)]
 struct Pos {
     at: ValveName,
     moving: Option<Movement>,
@@ -252,257 +218,7 @@ fn cost(
 }
 
 fn space_indent(budget: u8) {
-    (0..(26 - budget)).for_each(|_| print! {"|"});
-}
-
-fn move_pos(src: ValveName, mov: &Movement, next: bool, new_cost: u8) -> Pos {
-    if next {
-        // pos reached
-        Pos {
-            at: mov.dest,
-            moving: None,
-        }
-    } else {
-        Pos {
-            at: src,
-            moving: Some(Movement {
-                dest: mov.dest,
-                flow: mov.flow,
-                cost: mov.cost - new_cost,
-            }),
-        }
-    }
-}
-
-fn sub_vec2<T>(v: &[T], rm1: Option<usize>, rm2: Option<usize>) -> Vec<T>
-where
-    T: Clone,
-{
-    let mut v2 = v.to_vec();
-    match (rm1, rm2) {
-        (None, None) => {}
-        (None, Some(r)) | (Some(r), None) => {
-            v2.swap_remove(r);
-        }
-        (Some(r1), Some(r2)) => {
-            if r1 > r2 {
-                v2.swap_remove(r1);
-                v2.swap_remove(r2);
-            } else {
-                v2.swap_remove(r2);
-                v2.swap_remove(r1);
-            }
-        }
-    }
-    v2
-}
-
-fn max_flow_double(
-    valves: &HashMap<ValveName, Valve>,
-    pos: &[Pos; 2],
-    remaining: &[ValveName],
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    /*
-    space_indent(budget);
-    println!(
-        "budget is {budget} from {pos:?}: remain {} :{remaining:?} ",
-        remaining.len()
-    );
-    */
-    match (&pos[0].moving, &pos[1].moving) {
-        (None, None) => {
-            if pos[0].at != pos[1].at {
-                max_flow_double_combine_all(
-                    valves, pos[0].at, pos[1].at, remaining, budget, path_memo,
-                )
-            } else {
-                max_flow_double_combine(valves, pos[0].at, pos[1].at, remaining, budget, path_memo)
-            }
-        }
-        (None, Some(m)) => max_flow_double_iter(
-            valves, pos[0].at, pos[1].at, m, remaining, budget, path_memo,
-        ),
-        (Some(m), None) => max_flow_double_iter(
-            valves, pos[1].at, pos[0].at, m, remaining, budget, path_memo,
-        ),
-        (Some(_), Some(_)) => unreachable!(),
-    }
-}
-
-fn max_flow_double_iter(
-    valves: &HashMap<ValveName, Valve>,
-    at: ValveName,
-    atm: ValveName,
-    m: &Movement,
-    remaining: &[ValveName],
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    /*
-    space_indent(budget);
-    println!("Iter over {remaining:?} ({})", remaining.len());
-    */
-    if remaining.is_empty() && m.cost < budget {
-        /*
-        space_indent(budget);
-        println!("Exhausting");
-        */
-        return flow_exhaust(m, budget);
-    }
-    let mut max = 0;
-    // this could be an iterator...
-    for i in 0..remaining.len() {
-        let dest = remaining[i];
-        let cost = cost(at, dest, valves, path_memo);
-        let flow = valves.get(&dest).expect("valve").flow;
-
-        let new_flow = max_flow_double_pair(
-            valves,
-            &[at, atm],
-            [&Movement { dest, flow, cost }, m],
-            &sub_vec2(remaining, Some(i), None),
-            budget,
-            path_memo,
-        );
-        if new_flow > max {
-            max = new_flow;
-        }
-    }
-    max
-}
-
-fn max_flow_double_combine_all(
-    valves: &HashMap<ValveName, Valve>,
-    at_1: ValveName,
-    at_2: ValveName,
-    remaining: &[ValveName],
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    /*
-    space_indent(budget);
-    println!("Combining all over {remaining:?} ({})", remaining.len());
-    */
-    let mut max = 0;
-    for v in (0..remaining.len()).combinations(2) {
-        let i = v[0];
-        let j = v[1];
-        let new_flow = flow_pair(valves, at_1, at_2, remaining, i, j, budget, path_memo);
-        if new_flow > max {
-            max = new_flow;
-        }
-        let new_flow = flow_pair(valves, at_1, at_2, remaining, j, i, budget, path_memo);
-        if new_flow > max {
-            max = new_flow;
-        }
-    }
-    max
-}
-fn max_flow_double_combine(
-    valves: &HashMap<ValveName, Valve>,
-    at_1: ValveName,
-    at_2: ValveName,
-    remaining: &[ValveName],
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    /*
-    space_indent(budget);
-    println!("Combining over {remaining:?} ({})", remaining.len());
-    */
-    let mut max = 0;
-    for v in (0..remaining.len()).combinations(2) {
-        let i = v[0];
-        let j = v[1];
-        let new_flow = flow_pair(valves, at_1, at_2, remaining, i, j, budget, path_memo);
-        if new_flow > max {
-            max = new_flow;
-        }
-    }
-    max
-}
-fn flow_pair(
-    valves: &HashMap<ValveName, Valve>,
-    at_1: ValveName,
-    at_2: ValveName,
-    remaining: &[ValveName],
-    i: usize,
-    j: usize,
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    let desti = remaining[i];
-    let costi = cost(at_1, desti, valves, path_memo);
-    let flowi = valves.get(&desti).expect("valve").flow;
-    let destj = remaining[j];
-    let costj = cost(at_1, destj, valves, path_memo);
-    let flowj = valves.get(&destj).expect("valve").flow;
-
-    max_flow_double_pair(
-        valves,
-        &[at_1, at_2],
-        [
-            &Movement {
-                dest: desti,
-                flow: flowi,
-                cost: costi,
-            },
-            &Movement {
-                dest: destj,
-                flow: flowj,
-                cost: costj,
-            },
-        ],
-        &sub_vec2(remaining, Some(i), Some(j)),
-        budget,
-        path_memo,
-    )
-}
-fn max_flow_double_pair(
-    valves: &HashMap<ValveName, Valve>,
-    src: &[ValveName; 2],
-    mov: [&Movement; 2],
-    remaining: &[ValveName],
-    budget: u8,
-    path_memo: &mut HashMap<(ValveName, ValveName), u8>,
-) -> usize {
-    if mov[0].cost >= budget && mov[1].cost >= budget {
-        return 0;
-    }
-    let (next, next_cost) = match mov[0].cost.cmp(&mov[1].cost) {
-        std::cmp::Ordering::Less => ([true, false], mov[0].cost),
-        std::cmp::Ordering::Equal => ([true, true], mov[0].cost),
-        std::cmp::Ordering::Greater => ([false, true], mov[1].cost),
-    };
-    let (new_budget, flow, new_pos) = (
-        budget - next_cost,
-        if next[0] { mov[0].flow } else { 0 } + if next[1] { mov[1].flow } else { 0 },
-        [
-            move_pos(src[0], mov[0], next[0], next_cost),
-            move_pos(src[1], mov[1], next[1], next_cost),
-        ],
-    );
-
-    /*
-    space_indent(budget);
-    println!("pos {src:?}->{new_pos:?} rest: {remaining:?}");
-    */
-    let mflow = max_flow_double(valves, &new_pos, remaining, new_budget, path_memo);
-    let new_flow = (new_budget as usize) * flow + mflow;
-    /*
-    space_indent(budget);
-    println!(
-                "= has flow {new_flow} = {new_budget} * {flow} + {mflow} at step {} with {src:?}->{new_pos:?}",
-                27 - budget
-            );
-    */
-    new_flow
-}
-
-fn flow_exhaust(m: &Movement, budget: u8) -> usize {
-    m.flow * (budget as usize - m.cost as usize)
+    (0..(26 - budget)).for_each(|_| print! {" "});
 }
 
 fn path_to_valve(valves: &HashMap<ValveName, Valve>, start: ValveName, target: ValveName) -> u8 {
@@ -554,8 +270,127 @@ fn path_to_valve(valves: &HashMap<ValveName, Valve>, start: ValveName, target: V
             });
         }
     }
+    //return *mincost.get(&target).unwrap_or(&u8::MAX);
     u8::MAX
 }
+
+type BitFlow = HashMap<u64, usize>;
+type ValveMap = HashMap<ValveName, Valve>;
+type PathMemo = HashMap<(ValveName, ValveName), u8>;
+
+fn max_flow_with_elephant_fast(valves: &ValveMap) -> usize {
+    let mut path_memo = HashMap::new();
+    let valves_with_flow: Vec<ValveName> = valves
+        .iter()
+        .filter(|(_, v)| v.flow > 0)
+        .map(|(name, v)| *name)
+        .collect();
+    let valve_bits: HashMap<ValveName, u64> = valves_with_flow
+        .iter()
+        .scan(0, |acc, valve| {
+            *acc += 1;
+            Some((*valve, 1 << (*acc - 1)))
+        })
+        .collect();
+    /*
+    valve_bits.iter().for_each(|(v, b)| {
+        println!("{v} has bit {b:08X}");
+    });
+    */
+    let mut bitflow = BitFlow::new();
+    flow_all(
+        valves,
+        &valve_bits,
+        &mut path_memo,
+        &valves_with_flow,
+        &mut bitflow,
+        "AA".into(),
+        26,
+        0,
+        0,
+    );
+    bitflow
+        .iter()
+        .flat_map(|(s1, f1)| {
+            bitflow
+                .iter()
+                .filter(|(s2, _)| (*s1 & *s2) == 0)
+                .map(|(_, f2)| *f1 + *f2)
+        })
+        .max()
+        .expect("max")
+}
+fn flow_all(
+    valves: &ValveMap,
+    valve_bits: &HashMap<ValveName, u64>,
+    path_memo: &mut PathMemo,
+    remain: &[ValveName],
+    bitflow: &mut BitFlow,
+    at: ValveName,
+    budget: u8,
+    state: u64,
+    flow: usize,
+) {
+    /*
+    if matches!(bitflow.get(&state), Some(f)  if *f > flow ) {
+        return;
+    }
+    */
+    let f = bitflow.get(&state).unwrap_or(&0);
+    bitflow.insert(state, max(*f, flow));
+
+    for dest in remain.iter().cloned() {
+        /*
+        space_indent(budget);
+        println!(
+            "Evaluating {at}({}) -> {dest}({}) (state: {state:08X}",
+            at.bit(),
+            dest.bit(),
+        );
+        */
+        let db = valve_bits.get(&dest).expect("a bitmask");
+        if db & state != 0 {
+            continue;
+        }
+        let v = valves.get(&dest).expect("dest");
+        let cost = cost(at, dest, valves, path_memo);
+        if cost >= budget {
+            continue;
+        }
+        let new_budget = budget - cost;
+        flow_all(
+            valves,
+            valve_bits,
+            path_memo,
+            remain,
+            bitflow,
+            dest,
+            new_budget,
+            state | db,
+            flow + (new_budget as usize * v.flow),
+        );
+    }
+}
+/*
+fn sub_vec<T>(v: &[T], removed: usize) -> Vec<T>
+where
+    T: Clone,
+{
+    assert!(removed < v.len());
+    if v.is_empty() {
+        Vec::new()
+    } else if removed == 0 {
+        v[1..v.len()].to_vec()
+    } else if removed + 1 == v.len() {
+        v[0..removed].to_vec()
+    } else {
+        let mut v2 = Vec::with_capacity(v.len() - 1);
+        v2.extend_from_slice(&v[0..removed]);
+        v2.extend_from_slice(&v[(removed + 1)..(v.len())]);
+        v2
+    }
+}
+*/
 
 #[test]
 fn test() {
@@ -564,7 +399,7 @@ fn test() {
     let res = most_30m_pressure(&valves);
     assert_eq!(res, 1651);
     //part 2
-    let res = max_flow_with_elephant(&valves);
+    let res = max_flow_with_elephant_fast(&valves);
     assert_eq!(res, 1707);
 }
 
@@ -683,6 +518,7 @@ Valve CZ has flow rate=10; tunnels lead to valves CK",
     {
         let valves = parse(input);
         assert_eq!(most_30m_pressure(&valves), *p1, "Part 1 failed");
-        assert_eq!(max_flow_with_elephant(&valves), *p2, "Part 2 failed");
+        assert_eq!(max_flow_with_elephant_fast(&valves), *p2, "Part 2 failed");
+        println!("=========================================== OK !!!!!!!!");
     }
 }
