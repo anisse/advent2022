abc726916ce3ec2efc6c50cffdddb0e354fb1d66
Anisse Astier <anisse@astier.eu>
2022-12-18 06:28:15 +0100

    day18: part 1

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+
 use advent2022::*;
 fn main() {
     let cubes = parse(input!());
     //part 1
-    let res = operation(&cubes);
+    let res = unexposed_surface(&cubes);
     println!("Summary: {}", res);
     //part 2
     //let res = operation2(&cubes);
@@ -17,22 +19,34 @@ fn parse(input: &str) -> Vec<Cube> {
         .map(|l| l.split(',').map(|x| x.parse().expect("not int")).collect())
         .collect()
 }
-fn operation(cubes: &[Cube]) -> usize {
-    let mut count = 0;
-    for _ in cubes.iter() {
-        if true {
-            count += 1
-        }
-        todo!()
+fn unexposed_surface(cubes: &[Cube]) -> usize {
+    let mut surfaces_xy: HashMap<(u8, u8, u8), u8> = HashMap::new();
+    let mut surfaces_xz: HashMap<(u8, u8, u8), u8> = HashMap::new();
+    let mut surfaces_yz: HashMap<(u8, u8, u8), u8> = HashMap::new();
+    for c in cubes.iter() {
+        let x = c[0];
+        let y = c[1];
+        let z = c[2];
+        *surfaces_xy.entry((x, y, z)).or_insert(0) += 1;
+        *surfaces_xy.entry((x, y, z + 1)).or_insert(0) += 1;
+        *surfaces_xz.entry((x, y, z)).or_insert(0) += 1;
+        *surfaces_xz.entry((x, y + 1, z)).or_insert(0) += 1;
+        *surfaces_yz.entry((x, y, z)).or_insert(0) += 1;
+        *surfaces_yz.entry((x + 1, y, z)).or_insert(0) += 1;
     }
-    count
+    surfaces_xy
+        .values()
+        .chain(surfaces_xz.values())
+        .chain(surfaces_yz.values())
+        .filter(|v| **v == 1)
+        .count()
 }
 
 #[test]
 fn test() {
     let cubes = parse(sample!());
     //part 1
-    let res = operation(&cubes);
+    let res = unexposed_surface(&cubes);
     assert_eq!(res, 64);
     //part 2
     // let res = operation2(&cubes);
