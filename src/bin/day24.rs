d743fb383fca9d62b34550056016d6ec1908f080
Anisse Astier <anisse@astier.eu>
2022-12-24 07:53:07 +0100

    day23: part 1 possibly working but too slow. bad algo ?

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -3,12 +3,12 @@ use advent2022::*;
 use crate::Tile::*;
 
 fn main() {
-    let things = parse(input!());
+    let map = parse(input!());
     //part 1
-    let res = operation(&things);
+    let res = fastest_path(&map);
     println!("Summary: {}", res);
     //part 2
-    //let res = operation2(&things);
+    //let res = operation2(&map);
     //println!("Summary2: {}", res);
 }
 fn parse(input: &str) -> Map {
@@ -35,7 +35,7 @@ fn parse(input: &str) -> Map {
 type Map = Vec<Vec<Tile>>;
 type MapSlice = [Vec<Tile>];
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Tile {
     Up,
     Down,
@@ -43,31 +43,106 @@ enum Tile {
     Right,
     Empty,
 }
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Blizzard {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+impl From<usize> for Blizzard {
+    fn from(value: usize) -> Self {
+        match value % 4 {
+            0 => Self::Up,
+            1 => Self::Down,
+            2 => Self::Left,
+            3 => Self::Right,
+            _ => unreachable!(),
+        }
+    }
+}
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Copy)]
 struct Pos {
     x: u8,
     y: u8,
 }
-
-fn operation(things: &MapSlice) -> usize {
-    let mut count = 0;
-    for _ in things.iter() {
-        if true {
-            count += 1
+impl Pos {
+    fn adjacent_and_self(self, map_max: Pos) -> Vec<Pos> {
+        let mut v = vec![];
+        for (vx, vy) in [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)].iter() {
+            let new_x = self.x as i16 + vx;
+            let new_y = self.y as i16 + vy;
+            if new_x < 0 || new_x >= map_max.x as i16 || new_y < 0 || new_y >= map_max.y as i16 {
+                continue;
+            }
+            v.push(Pos {
+                x: new_x as u8,
+                y: new_y as u8,
+            });
         }
-        todo!()
+        v
     }
-    count
+}
+
+fn fastest_path(map: &MapSlice) -> usize {
+    let mut next_pos = vec![Pos { x: 0, y: 0 }];
+    let mut new_next = vec![];
+    let mut step = 0;
+    let map_max = Pos {
+        x: map[0].len() as u8,
+        y: map.len() as u8,
+    };
+    let target_pos = Pos {
+        x: map[0].len() as u8 - 1,
+        y: map.len() as u8 - 1,
+    };
+    'outer: loop {
+        step += 1;
+        while let Some(nextp) = next_pos.pop() {
+            //println!("At step {step} evaluating {nextp:?}");
+            if has_blizzard(map, &nextp, step) {
+                //   println!("Is estimated to have blizzard");
+                continue;
+            }
+            if nextp == target_pos {
+                break 'outer;
+            }
+            for p in nextp.adjacent_and_self(map_max).iter() {
+                new_next.push(*p);
+            }
+        }
+        (next_pos, new_next) = (new_next, next_pos);
+    }
+    step + 1
+}
+
+fn has_blizzard(map: &MapSlice, p: &Pos, round: usize) -> bool {
+    (0..4).map(Blizzard::from).any(|b| match b {
+        Blizzard::Up => {
+            map[(p.y as usize + map.len() + round) % map.len()][p.x as usize] == Tile::Up
+        }
+        Blizzard::Down => {
+            map[(p.y as usize + map.len() * 1024 - round) % map.len()][p.x as usize] == Tile::Down
+        }
+        Blizzard::Left => {
+            map[p.y as usize][(p.x as usize + map[0].len() + round) % map[0].len()] == Tile::Left
+        }
+        Blizzard::Right => {
+            map[p.y as usize][(p.x as usize + map[0].len() * 1024 - round) % map[0].len()]
+                == Tile::Right
+        }
+    })
 }
 
 #[test]
 fn test() {
-    let things = parse(sample!());
+    let map = parse(sample!());
+    dbg!(&map);
     //part 1
-    let res = operation(&things);
-    assert_eq!(res, 42);
+    let res = fastest_path(&map);
+    assert_eq!(res, 18);
     //part 2
-    // let res = operation2(&things);
+    // let res = operation2(&map);
     // assert_eq!(res, 42);
 }
