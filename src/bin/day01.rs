a370625732556a8b7ff0a5e1716d16c597b74775
Anisse Astier <anisse@astier.eu>
2023-11-26 19:17:29 +0100

    day01: experimenting a bit with full iterators
    It's not that ergonomic, but that can be solved by a good template.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -3,41 +3,40 @@ use advent2022::*;
 fn main() {
     let elfcalories = parse(input!());
     //part 1
-    let res = max_calories(&elfcalories);
+    let res = max_calories(elfcalories.clone());
     println!("Max calories: {}", res);
     //part 2
-    let res = top3_max(&elfcalories);
+    let res = top3_max(elfcalories);
     println!("Top 3: {}", res);
 }
-fn parse(input: &str) -> Vec<Vec<usize>> {
+
+fn parse(input: &str) -> impl Iterator<Item = Vec<usize>> + '_ + Clone {
     input
         .split("\n\n")
         .map(|elf| elf.lines().map(|x| x.parse().expect("not int")).collect())
-        .collect()
 }
-fn max_calories(elfcalories: &[Vec<usize>]) -> usize {
-    elfcalories
-        .iter()
-        .map(|v| v.iter().sum())
-        .max()
-        .expect("no max")
+fn max_calories<I>(elfcalories: I) -> usize
+where
+    I: Iterator<Item = Vec<usize>>,
+{
+    elfcalories.map(|v| v.iter().sum()).max().expect("no max")
 }
 
-fn top3_max(elfcalories: &[Vec<usize>]) -> usize {
-    let mut sorted_sum = elfcalories
-        .iter()
-        .map(|v| v.iter().sum())
-        .collect::<Vec<usize>>();
+fn top3_max<I>(elfcalories: I) -> usize
+where
+    I: Iterator<Item = Vec<usize>>,
+{
+    let mut sorted_sum = elfcalories.map(|v| v.iter().sum()).collect::<Vec<usize>>();
     sorted_sum.sort_by(|a, b| b.cmp(a));
     sorted_sum.iter().take(3).sum()
 }
 #[test]
 fn test() {
-    let elfcalories = parse(sample!());
+    let mut elfcalories = parse(sample!());
     //part 1
-    let res = max_calories(&elfcalories);
+    let res = max_calories(elfcalories.clone());
     assert_eq!(res, 24000);
     //part 2
-    let res = top3_max(&elfcalories);
+    let res = top3_max(&mut elfcalories);
     assert_eq!(res, 24000 + 11000 + 10000);
 }
