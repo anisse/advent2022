dca14d1f7990b181d3dbff5d310e6a9fcc879440
Anisse Astier <anisse@astier.eu>
2023-11-27 22:41:07 +0100

    template: iter-ify

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -2,18 +2,22 @@ use advent2022::*;
 fn main() {
     let things = parse(input!());
     //part 1
-    let res = operation(&things);
-    println!("Summary: {}", res);
+    let res = operation(things.clone());
+    println!("Part 1: {}", res);
     //part 2
-    //let res = operation2(&things);
-    //println!("Summary2: {}", res);
+    let res = operation2(things);
+    println!("Part 2: {}", res);
 }
-fn parse(input: &str) -> Vec<u8> {
-    input.lines().map(|x| x.parse().expect("not int")).collect()
+type ParsedItem = u8;
+fn parse(input: &str) -> impl Iterator<Item = ParsedItem> + Clone + '_ {
+    input.lines().map(|x| x.parse().expect("not int"))
 }
-fn operation(things: &[u8]) -> usize {
+fn operation<I>(things: I) -> usize
+where
+    I: Iterator<Item = ParsedItem>,
+{
     let mut count = 0;
-    for _ in things.iter() {
+    for _ in things {
         if true {
             count += 1
         }
@@ -22,13 +26,23 @@ fn operation(things: &[u8]) -> usize {
     count
 }
 
+fn operation2<I>(things: I) -> usize
+where
+    I: Iterator<Item = ParsedItem>,
+{
+    for _ in things {
+        todo!()
+    }
+    0
+}
+
 #[test]
 fn test() {
     let things = parse(sample!());
     //part 1
-    let res = operation(&things);
+    let res = operation(things.clone());
     assert_eq!(res, 42);
     //part 2
-    // let res = operation2(&things);
-    // assert_eq!(res, 42);
+    let res = operation2(things);
+    assert_eq!(res, 42);
 }
